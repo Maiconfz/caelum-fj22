7d9617f9a4b9bacd7e8fa75912e1007c7a2908b0
Maicon Fonseca Zanco <maiconfz@gmail.com>
2017-02-05 19:26:29 -0200

    Criar mais classes de teste para o CandlestickFactory

---
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/testes/TestaCandlestickFactorySemNegociacoes.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package br.com.caelum.argentum.testes;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import br.com.caelum.argentum.modelo.Candlestick;
+import br.com.caelum.argentum.modelo.CandlestickFactory;
+import br.com.caelum.argentum.modelo.Negociacao;
+
+/**
+ * @author maiconfz
+ *
+ */
+public class TestaCandlestickFactorySemNegociacoes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calendar hoje = Calendar.getInstance();
+
+		List<Negociacao> negociacoes = Arrays.asList();
+		
+		CandlestickFactory fabrica = new CandlestickFactory();
+		Candlestick candle = fabrica.constroiCandleParaData(hoje, negociacoes);
+		
+		System.out.println(candle);
+	}
+
+}
