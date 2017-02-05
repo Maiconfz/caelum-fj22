24658733d2d10e6fd4b3d5b87dabf6990d1aeecb
Maicon Fonseca Zanco <maiconfz@gmail.com>
2017-02-05 21:08:52 -0200

    Corrigir CandlestickFactory para tratar criação de candle
    Permitir negocições vazias ou com apenas uma negociação

--- fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -11,24 +11,26 @@ import java.util.List;
  *
  */
 public class CandlestickFactory {
-	public Candlestick constroiCandleParaData(Calendar data, List<Negociacao> negociacoes){
-		double maximo = negociacoes.get(0).getPreco();
-		double minimo = negociacoes.get(negociacoes.size()-1).getPreco();
+	public Candlestick constroiCandleParaData(Calendar data, List<Negociacao> negociacoes) {
+		double maximo = 0;
+		double minimo = negociacoes.isEmpty() ? 0 : Double.MAX_VALUE;
 		double volume = 0;
-		
+
 		for (Negociacao negociacao : negociacoes) {
 			volume += negociacao.getVolume();
-			
-			if(negociacao.getPreco()>maximo) {
+
+			if (negociacao.getPreco() > maximo) {
 				maximo = negociacao.getPreco();
-			}else if(negociacao.getPreco()<minimo){
+			}
+
+			if (negociacao.getPreco() < minimo) {
 				minimo = negociacao.getPreco();
 			}
 		}
-		
-		double abertura = negociacoes.get(0).getPreco();
-		double fechamento = negociacoes.get(negociacoes.size()-1).getPreco();
-		
+
+		double abertura = negociacoes.isEmpty() ? 0 : negociacoes.get(0).getPreco();
+		double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(negociacoes.size() - 1).getPreco();
+
 		return new Candlestick(abertura, fechamento, minimo, maximo, volume, data);
 	}
 }
