eae006fa3e49f4b29fbc643c481b1a64aecdda5d
Maicon Fonseca Zanco <maiconfz@gmail.com>
2017-02-05 18:07:32 -0200

    Adicionar classes do modelo do projeto fj22-argentum
    Criar classes Negociacao e Candlestick e arquivo .gitignore

---
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/Candlestick.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package br.com.caelum.argentum.modelo;
+
+import java.util.Calendar;
+
+/**
+ * @author maiconfz
+ *
+ */
+public final class Candlestick {
+	private final double abertura;
+	private final double fechamento;
+	private final double minimo;
+	private final double maximo;
+	private final double volume;
+	private final Calendar data;
+
+	/**
+	 * @param abertura
+	 * @param fechamento
+	 * @param minimo
+	 * @param maximo
+	 * @param volume
+	 * @param data
+	 */
+	public Candlestick(double abertura, double fechamento, double minimo, double maximo, double volume, Calendar data) {
+		this.abertura = abertura;
+		this.fechamento = fechamento;
+		this.minimo = minimo;
+		this.maximo = maximo;
+		this.volume = volume;
+		this.data = data;
+	}
+
+	/**
+	 * @return the abertura
+	 */
+	public double getAbertura() {
+		return abertura;
+	}
+
+	/**
+	 * @return the fechamento
+	 */
+	public double getFechamento() {
+		return fechamento;
+	}
+
+	/**
+	 * @return the minimo
+	 */
+	public double getMinimo() {
+		return minimo;
+	}
+
+	/**
+	 * @return the maximo
+	 */
+	public double getMaximo() {
+		return maximo;
+	}
+
+	/**
+	 * @return the volume
+	 */
+	public double getVolume() {
+		return volume;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Calendar getData() {
+		return data;
+	}
+	
+	public boolean isAlta(){
+		return this.abertura < this.fechamento;
+	}
+	
+	public boolean isBaixa(){
+		return this.fechamento > this.abertura;
+	}
+}
