c551811e6e1243b0c3c4db0c601c679d8197c547
Maicon Fonseca Zanco <maiconfz@gmail.com>
2017-02-05 22:28:20 -0200

    Alterar CandleBuilder para lançar exceção
    Lança quando nem todos os métodos necessários foram chamados

--- fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandleBuilder.java
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandleBuilder.java
@@ -17,37 +17,54 @@ public class CandleBuilder {
 	private double volume;
 	private Calendar data;
 
+	private boolean aberturaSetada;
+	private boolean fechamentoSetado;
+	private boolean minimoSetado;
+	private boolean maximoSetado;
+	private boolean volumeSetado;
+	private boolean dataSetada;
+
 	public CandleBuilder comAbertura(double abertura) {
 		this.abertura = abertura;
+		this.aberturaSetada = true;
 		return this;
 	}
 
 	public CandleBuilder comFechamento(double fechamento) {
 		this.fechamento = fechamento;
+		this.fechamentoSetado = true;
 		return this;
 	}
 
 	public CandleBuilder comMinimo(double minimo) {
 		this.minimo = minimo;
+		this.minimoSetado = true;
 		return this;
 	}
 
 	public CandleBuilder comMaximo(double maximo) {
 		this.maximo = maximo;
+		this.maximoSetado = true;
 		return this;
 	}
 
 	public CandleBuilder comVolume(double volume) {
 		this.volume = volume;
+		this.volumeSetado = true;
 		return this;
 	}
 
 	public CandleBuilder comData(Calendar data) {
 		this.data = data;
+		this.dataSetada = true;
 		return this;
 	}
 
 	public Candlestick geraCandle() {
-		return new Candlestick(this.abertura, this.fechamento, this.minimo, this.maximo, this.volume, this.data);
+		if (aberturaSetada && fechamentoSetado && minimoSetado && maximoSetado && volumeSetado && dataSetada) {
+			return new Candlestick(this.abertura, this.fechamento, this.minimo, this.maximo, this.volume, this.data);
+		} else {
+			throw new IllegalStateException("Todos os valores dever ser setados para a construção do candle");
+		}
 	}
 }
